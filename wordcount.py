baa6f057b4e203f145a42677dd5dd53188cdce1b
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 18:18:40 -0700

    New and improved, post code review.

--- wordcount.py
+++ wordcount.py
@@ -2,7 +2,7 @@
 
 import string
 
-from operator import itemgetter
+
 from sys import argv
 
 script, filename = argv
@@ -21,17 +21,24 @@ def wordcount(filename):
                 word_counter[word] = 1
             else:
                 word_counter[word] +=1
+    #close file
     return word_counter
 
 
 def sort_dict_freq(my_dict):
     "Sorts dictionary in descending order of value and alphabetical within values"
+    my_dict = my_dict.items()
 
-    sorted_by_freq_then_letter = sorted(my_dict.items(), key = itemgetter(1,0), reverse = False)
-    for item in sorted_by_freq_then_letter:
-        word = item[0]
-        numb_times = item[1]
-        print "%s appears %r times" % (word,numb_times)
+    my_dict_into_reverse_tuple = []
+    for i in range(len(my_dict)):
+        word = my_dict[i][0]
+        count = my_dict[i][1]
+        my_dict_into_reverse_tuple.append((count, word))
+
+    sorted_out_of_place = sorted(my_dict_into_reverse_tuple)
+
+    for i in range(len(sorted_out_of_place)):
+        print sorted_out_of_place[i][1], sorted_out_of_place[i][0]
 
 
 sort_dict_freq(wordcount(filename))
